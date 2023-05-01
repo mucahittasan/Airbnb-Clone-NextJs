cba46b3246ba27da6eeb75ec57ff281db57619e1
mucahittasan <mucahittasan0@gmail.com>
2023-05-01 18:41:37 +0300

    Test commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,6 +10,7 @@ import prisma from "@/app/libs/prismadb"
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
+
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string
